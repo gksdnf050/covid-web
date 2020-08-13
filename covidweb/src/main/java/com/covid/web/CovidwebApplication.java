555d6720b676b559f661f3a4160b6d805f848250
201411146 <gksdnf050@naver.com>
2020-08-13 16:26:38 +0900

    # hanul 초기설정

---
+++ covidweb/src/main/java/com/covid/web/CovidwebApplication.java
@@ -0,0 +1,13 @@
+package com.covid.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidwebApplication.class, args);
+	}
+
+}
---
+++ covidweb/src/test/java/com/covid/web/CovidwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
