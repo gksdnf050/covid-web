87cb04b68e8da6ed9598ff3dab8deafaea5750e5
201411146 <gksdnf050@naver.com>
2020-08-29 23:04:10 +0900

    #hanul - 데이터베이스에서 안심restaurant정보 가져오기

--- covidweb/src/main/java/com/covid/web/controller/api/CovidController.java
+++ covidweb/src/main/java/com/covid/web/controller/api/CovidController.java
@@ -1,20 +1,24 @@
 package com.covid.web.controller.api;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.covid.web.util.KakaoUtil;
+import com.covid.web.dto.RestaurantDto;
+import com.covid.web.mapper.CovidMapper;
 import com.covid.web.util.RestaurantUtil;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class CovidController {
 	@Autowired
 	private RestaurantUtil restaurantUtil;
+	@Autowired
+	private CovidMapper covidMapper;
     @GetMapping("/hospital")
     public void getAllCovidHospital() throws IOException {
 
@@ -23,12 +27,9 @@ public class CovidController {
 
     @GetMapping("/restaurant")
     public void getCovidRestaurant() throws IOException {
-    	try {
-			restaurantUtil.getCovidRestaurant();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	List<RestaurantDto> dto = covidMapper.getRestaurant();
+    	
+    	//return dto;
     }
 
 }
--- covidweb/src/main/java/com/covid/web/mapper/CovidMapper.java
+++ covidweb/src/main/java/com/covid/web/mapper/CovidMapper.java
@@ -1,8 +1,11 @@
 package com.covid.web.mapper;
 
+import java.util.List;
+
 import com.covid.web.dto.RestaurantDto;
 
 public interface CovidMapper {
 
 	int insertRestaurant(RestaurantDto restaurantDto);
+	List<RestaurantDto> getRestaurant();
 }
